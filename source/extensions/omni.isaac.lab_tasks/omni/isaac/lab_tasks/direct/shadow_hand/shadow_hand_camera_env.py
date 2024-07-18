Fixes embedding calculation to allow for num_envs to not be a multiple of batch_size

source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/direct/shadow_hand/shadow_hand_camera_env.py
@@ -155,8 +155,9 @@ class ShadowHandCameraEnv(DirectRLEnv):
 
         with torch.no_grad():
             for i in range(0, self.num_envs, self.batch_size):
-                embeddings = self.model(transformed_img[i:i+self.batch_size]).view(self.batch_size, -1)
-                self.embeddings[i:i+self.batch_size, :] = embeddings
+                bound = min(self.batch_size+i, self.num_envs)
+                embeddings = self.model(transformed_img[i:bound]).view(bound-i, -1)
+                self.embeddings[i:bound, :] = embeddings
 
 
     def _pre_physics_step(self, actions: torch.Tensor) -> None: